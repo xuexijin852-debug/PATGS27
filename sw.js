ae567ee66f3edf67dfe304c69339c78f18a287fe
xuexijin852-debug <xuexijin852@gmail.com>
2026-09-21 13:08:09 +0900

    Update service worker version to 20260919c

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
    通知タップ時に「今日のPATGS」へ移動するようにしている。
    ========================================================= */
 
-const PATGS_SW_VERSION = "20260919b";
+const PATGS_SW_VERSION = "20260919c";
 
 
 self.addEventListener("install", function () {
